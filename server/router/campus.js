d0d2e21e3b85b9cffae333794ebcc6a7bab8efbb
1990Minds <1990minds@gmail.com>
2023-02-17 10:42:45 +0530

    live link updated

--- server/router/campus.js
+++ server/router/campus.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
-const {getOneCampus,getAllCampus,updateCampus,deleteCampus,createCampus} = require('../controller/campus')
+const {getOneCampus,getAllCampus,updateCampus,deleteCampus,createCampus,userCampus} = require('../controller/campus')
 
 router.get('/campus',getAllCampus)
 router.get('/campus/:id',getOneCampus)
 router.post('/campus',createCampus)
 router.put('/campus/:id',updateCampus)
 router.delete('/campus/:id',deleteCampus)
+router.get('/user-campus/:id',userCampus)
 
 module.exports = router
